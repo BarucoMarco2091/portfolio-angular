8f45e60a33367ad1ed87f2e2ba3dd296e76507c9
Marco <marcobaruco2091@gmail.com>
2025-02-24 19:13:00 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { FormComponent } from './form/form.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -9,7 +10,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent, FormsModule, CommonModule  ],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FormComponent, FooterComponent, FormsModule, CommonModule  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
