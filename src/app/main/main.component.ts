4a16371de334dfc81dfeabdf9e2a05ea898c1740
Marco <marcobaruco2091@gmail.com>
2025-02-24 16:38:30 -0300

    feat: main.component.html e main.scss

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [ FormsModule, CommonModule ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+  menuItems = [
+    { image: '/thumbnail-project-1-large.webp', name: 'DESIGN PORTFOLIO', tech: 'HTML, CSS'},
+    { image: '/thumbnail-project-2-large.webp', name: 'E-LEARNING LANDING PAGE', tech: 'HTML, CSS'},
+    { image: '/thumbnail-project-3-large.webp', name: 'TODO WEB APP', tech: 'HTML, CSS, JavaScript'},
+    { image: '/thumbnail-project-4-large.webp', name: 'ENTERTAINMENT WEB APP', tech: 'HTML, CSS, JavaScript'},
+    { image: '/thumbnail-project-5-large.webp', name: 'MEMORY GAME', tech: 'HTML, CSS, JavaScript'},
+    { image: '/thumbnail-project-6-large.webp', name: 'ART GALLERY SHOWCASE', tech: 'HTML, CSS, JavaScript'},
+  ]
+}
